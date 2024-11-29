442e75f5d637a52b18144af070dc1e305f539b2a
I539037 <jost-tomke.mueller@sap.com>
2024-11-29 12:24:40 +0200

    begin of state management

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,19 +1,21 @@
 import { Link } from "react-router-dom";
+import React, { useContext, useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { loadToken, getUserID, getBudgetID } from '../store/sessionStorage.js';
 import { getMonths, getPlan } from "../http-actions/http.js";
+import { PlanContext, PlanProvider } from "../store/PlanContext.js";
 
 import MainLayout from "./MainLayout.jsx";
 import Navbar from "../components/Navbar.jsx";
 import BarChart from "../components/BarChart.jsx";
 import LineChart from "../components/LineChart.jsx";
 import PieChart from "../components/PieChart.jsx";
-import { useEffect, useState } from "react";
 import Plan from "./Plan.jsx";
 
 const Dashboard = () => {
 
-    const [plan, setPlan] = useState([]);
+   // const [plan, setPlan] = useState([]);
+    const {plan, setPlan, updateMonthInPlan} = React.useContext(PlanContext);
     const [loading, setLoading] = useState(true);
     const [startBalance, setStartBalance] = useState(0);
     const [endBalance, setEndBalance] = useState(0);
@@ -76,62 +78,64 @@ const Dashboard = () => {
     if (isAuthenticated) {
         return (
             <>
-                <MainLayout userId={userID}>
-                    <Navbar userID={userID} currentPage={currentPage} />
-                    <div className="flex flex-row w-full px-4 py-1 gap-4">
-                        <div className="card card-bordered border-gray-300 min-w-60">
-                            <div className="card py-4 px-6">
-                                <h2 className="card-title text-xl">Start balance</h2>
-                                <p className="text-gray-500">Total</p>
-                                <div className="card-actions justify-start mt-4">
-                                    <h2 className="card-title text-4xl font-bold">{startBalance}€</h2>
+                <PlanProvider>
+                    <MainLayout userId={userID}>
+                        <Navbar userID={userID} currentPage={currentPage} />
+                        <div className="flex flex-row w-full px-4 py-1 gap-4">
+                            <div className="card card-bordered border-gray-300 min-w-60">
+                                <div className="card py-4 px-6">
+                                    <h2 className="card-title text-xl">Start balance</h2>
+                                    <p className="text-gray-500">Total</p>
+                                    <div className="card-actions justify-start mt-4">
+                                        <h2 className="card-title text-4xl font-bold">{startBalance}€</h2>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card card-bordered border-gray-300 min-w-60">
+                                <div className="card py-4 px-6">
+                                    <h2 className="card-title text-xl">End balance</h2>
+                                    <p className="text-gray-500">Total</p>
+                                    <div className="card-actions justify-start mt-4">
+                                        <h2 className="card-title text-4xl font-bold">{endBalance}€</h2>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="card card-bordered border-gray-300 min-w-60">
-                            <div className="card py-4 px-6">
-                                <h2 className="card-title text-xl">End balance</h2>
-                                <p className="text-gray-500">Total</p>
-                                <div className="card-actions justify-start mt-4">
-                                    <h2 className="card-title text-4xl font-bold">{endBalance}€</h2>
+                        <div className="flex flex-row w-full px-4 py-1 justify-between">
+                            <button className="btn rounded-3xl text-white bg-blue-600 hover:bg-blue-600 my-1" onClick={goToPlan}>
+                                <p>Go to plan</p>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M5 12h13M12 5l7 7-7 7" /></svg>
+                            </button>
+                            <div className="flex items-end justify-end">
+                                <div className="card my-1 mr-2 flex items-center">
+                                    <div className="flex gap-4">
+                                        <p className="text-md text-gray-500">Income and spending</p>
+                                        <input
+                                            type="checkbox"
+                                            className="toggle border-gray-300 bg-black [--tglbg:white] hover:bg-black"
+                                            defaultChecked
+                                            onClick={toggle}
+                                        />
+                                        <p className="text-md text-gray-500">Total balance</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="flex flex-row w-full px-4 py-1 justify-between">
-                        <button className="btn rounded-3xl text-white bg-blue-600 hover:bg-blue-600 my-1" onClick={goToPlan}>
-                            <p>Go to plan</p>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M5 12h13M12 5l7 7-7 7" /></svg>
-                        </button>
-                        <div className="flex items-end justify-end">
-                            <div className="card my-1 mr-2 flex items-center">
-                                <div className="flex gap-4">
-                                    <p className="text-md text-gray-500">Income and spending</p>
-                                    <input
-                                        type="checkbox"
-                                        className="toggle border-gray-300 bg-black [--tglbg:white] hover:bg-black"
-                                        defaultChecked
-                                        onClick={toggle}
-                                    />
-                                    <p className="text-md text-gray-500">Total balance</p>
-                                </div>
-                            </div>
+
                         </div>
 
-                    </div>
-
-                    <div className="flex flex-wrap w-full min-h-60 px-4 pb-4 pt-1 items-center">
-                        {bar && (
-                            <div className="card card-bordered border-gray-300 max-h-96 w-full p-6">
-                                <BarChart className="w-full h-full" />
-                            </div>)}
-                        {line && (
-                            <div className="card card-bordered border-gray-300 max-h-96 w-full p-6">
-                                <LineChart className="w-full h-full" />
-                            </div>
-                        )}
-                    </div>
-                </MainLayout>
+                        <div className="flex flex-wrap w-full min-h-60 px-4 pb-4 pt-1 items-center">
+                            {bar && (
+                                <div className="card card-bordered border-gray-300 max-h-96 w-full p-6">
+                                    <BarChart className="w-full h-full" />
+                                </div>)}
+                            {line && (
+                                <div className="card card-bordered border-gray-300 max-h-96 w-full p-6">
+                                    <LineChart className="w-full h-full" />
+                                </div>
+                            )}
+                        </div>
+                    </MainLayout>
+                </PlanProvider>
             </>
         )
     }
--- src/pages/Plan.jsx
+++ src/pages/Plan.jsx
@@ -7,7 +7,7 @@ import MainLayout from "./MainLayout.jsx";
 import Navbar from "../components/Navbar.jsx";
 import AddMonths from "../components/AddMonths.jsx";
 import { getBudgetID, getUserID } from "../store/sessionStorage.js";
-import { PlanContext } from "../store/PlanContext.js";
+import { PlanContext, PlanProvider } from "../store/PlanContext.js";
 
 const Plan = () => {
 
@@ -62,14 +62,16 @@ const Plan = () => {
 
     if (loading) {
         return (
-            <MainLayout userId={userID}>
-                <Navbar userId={userID} currentPage={currentPage} />
-                <div className="flex flex-row w-full h-full justify-center items-center px-4 py-1">
-                    <div className="flex justify-center items-center h-full">
-                        <p>Loading...</p>
+            <PlanProvider>
+                <MainLayout userId={userID}>
+                    <Navbar userId={userID} currentPage={currentPage} />
+                    <div className="flex flex-row w-full h-full justify-center items-center px-4 py-1">
+                        <div className="flex justify-center items-center h-full">
+                            <p>Loading...</p>
+                        </div>
                     </div>
-                </div>
-            </MainLayout >
+                </MainLayout >
+            </PlanProvider>
         );
     }
 
@@ -80,87 +82,89 @@ const Plan = () => {
 
     return (
         <>
-            <MainLayout userId={userID}>
-                <Navbar userId={userID} currentPage={currentPage} />
-                <div className="flex flex-row w-full px-4 py-1">
-                    <div className="stats stats-vertical lg:stats-horizontal card card-bordered border-gray-300">
-                        <div className="stat">
-                            <h2 className="card-title text-xl">Start balance</h2>
-                            <p className="text-gray-500">Total</p>
-                            <div className="card-actions justify-start mt-4">
-                                <h2 className="card-title text-4xl font-bold">{startBalance}€</h2>
+            <PlanProvider>
+                <MainLayout userId={userID}>
+                    <Navbar userId={userID} currentPage={currentPage} />
+                    <div className="flex flex-row w-full px-4 py-1">
+                        <div className="stats stats-vertical lg:stats-horizontal card card-bordered border-gray-300">
+                            <div className="stat">
+                                <h2 className="card-title text-xl">Start balance</h2>
+                                <p className="text-gray-500">Total</p>
+                                <div className="card-actions justify-start mt-4">
+                                    <h2 className="card-title text-4xl font-bold">{startBalance}€</h2>
+                                </div>
                             </div>
-                        </div>
-                        <div className="stat">
-                            <h2 className="card-title text-xl">End balance</h2>
-                            <p className="text-gray-500">Total</p>
-                            <div className="card-actions justify-start mt-4">
-                                <h2 className="card-title text-4xl font-bold">{endBalance}€</h2>
+                            <div className="stat">
+                                <h2 className="card-title text-xl">End balance</h2>
+                                <p className="text-gray-500">Total</p>
+                                <div className="card-actions justify-start mt-4">
+                                    <h2 className="card-title text-4xl font-bold">{endBalance}€</h2>
+                                </div>
                             </div>
-                        </div>
-                        <div className="stat">
-                            <h2 className="card-title text-xl">Assets</h2>
-                            <p className="text-gray-500">Total</p>
-                            <div className="card-actions justify-start mt-4">
-                                <h2 className="card-title text-4xl font-bold">0€</h2>
+                            <div className="stat">
+                                <h2 className="card-title text-xl">Assets</h2>
+                                <p className="text-gray-500">Total</p>
+                                <div className="card-actions justify-start mt-4">
+                                    <h2 className="card-title text-4xl font-bold">0€</h2>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                <div className="flex flex-row w-full h-16 py-1 items-center justify-between">
-                    <div className="flex justify-start px-6"><h2 className="text-xl font-bold">Plan view</h2></div>
-                    <div className="flex justify-end px-4">
-                        <button className="btn rounded-3xl btn-md shadow-sm border-gray-300 text-black bg-white hover:bg-white my-1"
-                            onClick={() => document.getElementById('add_months_modal').showModal()}>
-                            <p className="text-sm">Create new plan</p>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
-                        </button></div>
-                    <AddMonths />
-                </div>
-
-                <div className="card card-bordered border-gray-300 mx-4 mt-1 mb-4 min-w-60 min-h-96">
-                    <div className="overflow-x-auto">
-                        <table className="table table-sm bg-gray-50">
-                            {/* head */}
-                            <thead>
-                                <tr>
-                                    <th className="text-center py-4">MONTH</th>
-                                    <th className="text-center py-4">INCOME</th>
-                                    <th className="text-center py-4">SPENDING</th>
-                                    <th className="text-center py-4">SUM</th>
-                                    <th className="text-center py-4">TOTAL</th>
-                                    <th className="text-center py-4">ASSETS</th>
-                                    <th className="text-center py-4"></th>
-                                </tr>
-                            </thead>
-                            <tbody className="bg-white">
-
-                                {plan.map((month, index) => (
-                                    <tr className="hover:bg-gray-50" key={index} onClick={() => goToMonth(month)}>
-                                        <td className="text-center">{month.monthName}</td>
-                                        <td className="text-center"><div className="badge bg-green-100 text-green-800">{month.totalRevenue}€</div></td>
-                                        <td className="text-center"><div className="badge bg-red-100 text-red-800">{month.totalCosts}€</div></td>
-                                        <td className="text-center"><div className="badge">{month.sum}€</div></td>
-                                        <td className="text-center">{month.equity}€</td>
-                                        <td className="text-center">{month.assets}€</td>
-                                        <td className="text-center w-12">
-                                            <button className="btn btn-square btn-ghost btn-sm">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                                                    <circle cx="12" cy="12" r="1"></circle>
-                                                    <circle cx="19" cy="12" r="1"></circle>
-                                                    <circle cx="5" cy="12" r="1"></circle>
-                                                </svg>
-                                            </button>
-                                        </td>
-                                    </tr >
-                                ))}
-                            </tbody>
-                        </table>
+                    <div className="flex flex-row w-full h-16 py-1 items-center justify-between">
+                        <div className="flex justify-start px-6"><h2 className="text-xl font-bold">Plan view</h2></div>
+                        <div className="flex justify-end px-4">
+                            <button className="btn rounded-3xl btn-md shadow-sm border-gray-300 text-black bg-white hover:bg-white my-1"
+                                onClick={() => document.getElementById('add_months_modal').showModal()}>
+                                <p className="text-sm">Create new plan</p>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                            </button></div>
+                        <AddMonths />
                     </div>
-                    {/*<p>UserID: {userId}</p>*/}
-                </div>
-            </MainLayout >
+
+                    <div className="card card-bordered border-gray-300 mx-4 mt-1 mb-4 min-w-60 min-h-96">
+                        <div className="overflow-x-auto">
+                            <table className="table table-sm bg-gray-50">
+                                {/* head */}
+                                <thead>
+                                    <tr>
+                                        <th className="text-center py-4">MONTH</th>
+                                        <th className="text-center py-4">INCOME</th>
+                                        <th className="text-center py-4">SPENDING</th>
+                                        <th className="text-center py-4">SUM</th>
+                                        <th className="text-center py-4">TOTAL</th>
+                                        <th className="text-center py-4">ASSETS</th>
+                                        <th className="text-center py-4"></th>
+                                    </tr>
+                                </thead>
+                                <tbody className="bg-white">
+
+                                    {plan.map((month, index) => (
+                                        <tr className="hover:bg-gray-50" key={index} onClick={() => goToMonth(month)}>
+                                            <td className="text-center">{month.monthName}</td>
+                                            <td className="text-center"><div className="badge bg-green-100 text-green-800">{month.totalRevenue}€</div></td>
+                                            <td className="text-center"><div className="badge bg-red-100 text-red-800">{month.totalCosts}€</div></td>
+                                            <td className="text-center"><div className="badge">{month.sum}€</div></td>
+                                            <td className="text-center">{month.equity}€</td>
+                                            <td className="text-center">{month.assets}€</td>
+                                            <td className="text-center w-12">
+                                                <button className="btn btn-square btn-ghost btn-sm">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                                        <circle cx="12" cy="12" r="1"></circle>
+                                                        <circle cx="19" cy="12" r="1"></circle>
+                                                        <circle cx="5" cy="12" r="1"></circle>
+                                                    </svg>
+                                                </button>
+                                            </td>
+                                        </tr >
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                        {/*<p>UserID: {userId}</p>*/}
+                    </div>
+                </MainLayout >
+            </PlanProvider>
         </>
     )
 };
